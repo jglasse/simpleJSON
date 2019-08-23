26f0bd871106834c092ac52b837efc152ada0dee
jglasse <jeff@appetizeapp.com>
2019-08-22 17:32:17 -0700

    Initial Commit

---
+++ SimpleJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleJSON
+//
+//  Created by Jeff Glasse on 8/22/19.
+//  Copyright © 2019 Jeff Glasse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
